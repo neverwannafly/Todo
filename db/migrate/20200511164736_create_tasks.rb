699b4078c5ff9bf1ce7c66c9fc3eb78d4ba1d846
Shubham Anand <shubham_chess@live.com>
2020-05-11 22:18:52 +0530

    Add CRUD operations for Tasks
    This commit adds support for CRUD operations for tasks of the todo list. This template is generated via rails scaffolding.
    We also created a basic schema for the Todo app using rails scaffolding.

---
+++ db/migrate/20200511164736_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :labels
+      t.string :body
+      t.datetime :start_date
+      t.datetime :end_date
+
+      t.timestamps
+    end
+  end
+end
