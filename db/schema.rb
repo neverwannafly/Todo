0a7ba0ee227a2d9eb93655473bd2f03dbc1b0216
Shubham Anand <shubham_chess@live.com>
2020-05-12 14:00:52 +0530

    Add column completed to Tasks schema
    The following commit adds a new column to tasks schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_072104) do
+ActiveRecord::Schema.define(version: 2020_05_12_082936) do
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.string "labels"
-    t.text "body"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "priority"
-  end
+# Could not dump table "tasks" because of following StandardError
+#   Unknown type 'bool' for column 'completed'
 
 end
