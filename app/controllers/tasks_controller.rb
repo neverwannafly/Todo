483449a820e0823074c079539cc06723a7e4c123
Shubham Anand <shubham_chess@live.com>
2020-05-12 17:20:11 +0530

    Fix timer bugs and Add ordering
    This commit fixes a timer bug where all tasks share the same timer as well as adds ordering on the way tasks are retrieved. Higher priority tasks appear first in order of increasing time.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = Task.order(:completed, :priority, :end_date)
   end
 
   # GET /tasks/1
