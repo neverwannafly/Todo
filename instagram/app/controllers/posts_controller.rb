b0c85a3cf173bf50c48e9e0e6dfdcf5178df69df
Shubham Anand <shubham_chess@live.com>
2020-05-14 03:26:46 +0530

    implement delete functionality

--- instagram/app/controllers/posts_controller.rb
+++ instagram/app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -9,22 +11,32 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to posts_path
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: "Posted successfully." }
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
   def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully deleted.' }
+      format.json { head :no_content }
+    end
   end
 
 private
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
