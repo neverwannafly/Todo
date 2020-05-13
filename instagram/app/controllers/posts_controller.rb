0f407df2d147a4bfdbd5525b80fd21d7cfaf45fd
Shubham Anand <shubham_chess@live.com>
2020-05-13 21:31:43 +0530

    add capability to upload and show images

--- instagram/app/controllers/posts_controller.rb
+++ instagram/app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
   def index
-
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
+
 end
--- instagram/app/models/post.rb
+++ instagram/app/models/post.rb
@@ -1,5 +1,3 @@
 class Post < ApplicationRecord
-  validates :image, :presence => true
-  has_one_attached :image, :styles => { :medium => "640px" }
-  validates_attachement_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_one_attached :image
 end
