678649d783f28ff4992c4615f63ab4bc9461a7f8
Shubham Anand <shubham_chess@live.com>
2020-05-13 20:40:30 +0530

    add support for adding images

--- instagram/app/models/post.rb
+++ instagram/app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :image, :presence => true
+  has_one_attached :image, :styles => { :medium => "640px" }
+  validates_attachement_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
