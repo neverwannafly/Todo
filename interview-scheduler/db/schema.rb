15eadaf9605942addd8a6b9f0f2ba433d0065739
Shubham Anand <shubham_chess@live.com>
2020-05-15 19:19:51 +0530

    Add token to roles table

--- interview-scheduler/db/schema.rb
+++ interview-scheduler/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_134119) do
+ActiveRecord::Schema.define(version: 2020_05_15_134915) do
 
   create_table "roles", force: :cascade do |t|
     t.boolean "edit"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_05_15_134119) do
     t.boolean "view"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
   end
 
   create_table "users", force: :cascade do |t|
