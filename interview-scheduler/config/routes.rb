91adcb933773b8b1839e18ce9f3886640dd3169a
Shubham Anand <shubham_chess@live.com>
2020-05-15 19:47:59 +0530

    add views and routes

--- interview-scheduler/config/routes.rb
+++ interview-scheduler/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get   '/signup',  controller:'users',     action:'new'
+  post  '/signup',  controller:'users',     action:'create'
+  get   '/login',   controller:'sessions',  action:'new'
+  post  '/login',   controller:'sessions',  action:'create'
+  get   '/logout',  controller:'sessions',  action:'destroy'
 end
