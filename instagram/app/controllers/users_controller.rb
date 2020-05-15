ca6ee35f8750b66300bc5b6e340e9a269eb2247f
Shubham Anand <shubham_chess@live.com>
2020-05-15 05:53:53 +0530

    complete user login/register

--- instagram/app/controllers/users_controller.rb
+++ instagram/app/controllers/users_controller.rb
@@ -1,24 +1,30 @@
 class UsersController < ApplicationController
-  
+  # wrap_parameters :user, include: [:username, :email, :name, :password, :password_confirmation]
+
   def new
     @user = User.new
   end
 
   def create
-    user = User.new(
-      username: params[:username],
-      name: params[:name],
-      email: params[:email],
-      password_digest: params[:password],
-      password_confirmation: params[:password_confirmation],
-    )
-    if user.save
-      session[:user_id] = user.id
-      flash[:success] = "Successfully Created!"
-      redirect_to posts_url
+    @user = User.new user_params
+
+    if @user.save
+      respond_to do |format|
+        session[:user_id] = @user.id
+        format.html { redirect_to posts_url, notice: "Welcome #{@user.username}" }
+      end
     else
-      flash[:warning] = "Invalid Email or Password"
-      redirect_to '/signup'
+      puts @user.errors.full_messages
+      respond_to do |format|
+        format.html { redirect_to signup_url, notice: "Signup Failed!" }
+      end
     end
   end
+
+private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation, :email, :name, :username)
+  end
+
 end
