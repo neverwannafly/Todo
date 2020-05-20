3cce45a7ebcf3b58771809dbae674c6b8bc777ca
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-05-20 14:38:30 +0530

    add components to get and set user session

---
+++ interview-scheduler/src/services/GetUser.js
@@ -0,0 +1,9 @@
+const GetUser = {
+  userId: localStorage.getItem('userId'),
+  name: localStorage.getItem('name'),
+  email: localStorage.getItem('email'),
+  token: localStorage.getItem('token'),
+  username: localStorage.getItem('username'),
+};
+
+export default GetUser;
---
+++ interview-scheduler/src/services/SetUser.js
@@ -0,0 +1,15 @@
+const SetUser = async (user) => {
+  const userId = user.id,
+  const name = user.name;
+  const email = user.email;
+  const token = user.token;
+  const username = user.username;
+
+  localStorage.setItem('userId', userId);
+  localStorage.setItem('name', name);
+  localStorage.setItem('email', email);
+  localStorage.setItem('token', token);
+  localStorage.setItem('username', username);
+}
+
+export default SetUser;
