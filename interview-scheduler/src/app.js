e75e54fd0e9cbdbd9db2da6aeebb8b9fa5da6de6
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-05-20 08:06:50 +0530

    add router to event loop

--- interview-scheduler/src/app.js
+++ interview-scheduler/src/app.js
@@ -1,53 +1,25 @@
 "use strict";
 
-import Index        from './views/pages/Index.js';
-import About        from './views/pages/About.js'
-import Error404     from './views/pages/Error404.js'
-import PostShow     from './views/pages/PostShow.js'
-import Register     from './views/pages/Register.js'
+import Router from "./Router.js";
+import Error404 from "./views/pages/Error404.js";
 
-import Navbar       from './views/components/Navbar.js'
-import CreateInterviewModal from "./views/components/CreateInterviewModal.js";
+let router = new Router();
 
-import Utils        from './services/Utils.js'
-
-// List of supported routes. Any url other than these routes will throw a 404 error
-const routes = {
-    '/'           : Index,
-    '/about'      : About,
-    '/p/:id'      : PostShow,
-    '/register'   : Register,
-};
-
-
-// The router code. Takes a URLchecks against the list of supported routes and then renders the corresponding content page.
-const router = async () => {
-
-    // Lazy load view element:
-    const header = null || document.getElementById('header_container');
-    const content = null || document.getElementById('page_container');
-    const modal = null || document.getElementById('page_modal');
-    
-    // Render the Header and footer of the page
-    header.innerHTML = await Navbar.render(); await Navbar.after_render();
-    modal.innerHTML = await CreateInterviewModal.render(); await CreateInterviewModal.after_render();
-
-    // Get the parsed URl from the addressbar
-    let request = Utils.parseRequestURL()
-
-    // Parse the URL and if it has an id part, change it with the string ":id"
-    let parsedURL = (request.resource ? '/' + request.resource : '/') + (request.id ? '/:id' : '') + (request.verb ? '/' + request.verb : '')
-    
-    // Get the page from our hash of supported routes.
-    // If the parsed URL is not in our list of supported routes, select the 404 page instead
-    let page = routes[parsedURL] ? routes[parsedURL] : Error404
-    content.innerHTML = await page.render();
-    await page.after_render();
-  
+const renderPage = async () => {
+  let view = router.match();
+  console.log(view);
+  const root = document.getElementById('root');
+  if (view != null) {
+    root.innerHTML = await view.render();
+    await view.postRender();
+  } else {
+    root.innerHTML = await Error404.render();
+    await Error404.postRender();
+  }
 }
 
 // Listen on hash change:
-window.addEventListener('hashchange', router);
+window.addEventListener('hashchange', renderPage);
 
 // Listen on page load:
-window.addEventListener('load', router);
+window.addEventListener('load', renderPage);
